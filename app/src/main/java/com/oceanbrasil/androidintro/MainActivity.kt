9400bcdac9acdf3ed6e4bfac2981bb9c99590938
Mario Torres <scaramuzzini@gmail.com>
2024-03-03 17:49:31 -0400

    New button

--- app/src/main/java/com/oceanbrasil/androidintro/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/androidintro/MainActivity.kt
@@ -2,10 +2,23 @@ package com.oceanbrasil.androidintro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvTexto = findViewById<TextView>(R.id.texto)
+
+
+        val botao = findViewById<Button>(R.id.button2)
+        botao.setText("Ola!")
+        botao.setOnClickListener {
+            tvTexto.setText("outra coisa")
+            Toast.makeText(this,"clicou...",Toast.LENGTH_SHORT).show()
+        }
     }
 }
